0fffacf6ed51ebde6efd0ba875fd785ab824c748
Avarela314 <avarela0301@gmail.com>
2026-08-11 14:40:04 -0500

    Update camera.cpp
    working code for opening camera in gui
    
    header file for camera

--- KiloGui/src/camera.cpp
+++ KiloGui/src/camera.cpp
@@ -5,43 +5,53 @@
 #include <QPixmap>
 
 CameraWindow::CameraWindow(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(nullptr)   // nullptr makes this a separate top-level window
 {
     setWindowTitle("Kilobot Camera");
     resize(640, 480);
 
-    imageLabel = new QLabel(this);
+    imageLabel = new QLabel("Opening camera...", this);
     imageLabel->setAlignment(Qt::AlignCenter);
 
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(imageLabel);
-
-    camera.open(0);
 
     timer = new QTimer(this);
 
     connect(timer, &QTimer::timeout,
             this, &CameraWindow::updateFrame);
 
-    timer->start(30);
+    // Start updating the camera approximately 30 times per second
+    timer->start(33);
 }
 
 CameraWindow::~CameraWindow()
 {
+    timer->stop();
+
     if (camera.isOpened())
         camera.release();
 }
 
 void CameraWindow::updateFrame()
 {
-    if (!camera.isOpened())
-        return;
+    // Open the camera once
+    if (!camera.isOpened()) {
+        camera.open(0);
+
+        if (!camera.isOpened()) {
+            imageLabel->setText("Could not open camera");
+            return;
+        }
+    }
 
     cv::Mat frame;
     camera >> frame;
 
-    if (frame.empty())
+    if (frame.empty()) {
+        imageLabel->setText("Could not read camera frame");
         return;
+    }
 
     cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
 
